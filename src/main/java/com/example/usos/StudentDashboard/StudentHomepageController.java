32cefff34440eb0adf853923e84db399379ace97
zuzaflis <zuzaflis20@gmail.com>
2023-04-23 23:28:50 +0200

    it works!!!!

--- src/main/java/com/example/usos/StudentDashboard/StudentHomepageController.java
+++ src/main/java/com/example/usos/StudentDashboard/StudentHomepageController.java
@@ -1,15 +1,19 @@
 package com.example.usos.StudentDashboard;
 
+import com.example.usos.GradesPane;
 import com.example.usos.MainApp;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -21,15 +25,17 @@ public class StudentHomepageController {
 
     //----------------------
     @FXML
-    private void changeNodes() throws IOException {
+    private void changeNodes(String fxml) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        AnchorPane pane = loader.load(new FileInputStream(new File("src/main/resources/" + fxml)));
         Node newNode1 = new Label("New label 1");
         anchorPane.getChildren().clear();
-        anchorPane.getChildren().addAll(newNode1);
+        anchorPane.getChildren().addAll(pane);
 
     }
     //-----------------------------------------------------
     public void showGrades(ActionEvent actionEvent) throws IOException {
-        changeNodes();
+        changeNodes("com/example/usos/GradesPane.fxml");
     }
 
     //---------------------------------------------------
